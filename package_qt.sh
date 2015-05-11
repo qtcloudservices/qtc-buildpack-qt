810b0ca50441b1c35494b9185899ed9c8d572e32
Jari Kolehmainen <jari.kolehmainen@digia.com>
2015-05-11 10:49:06 +0300

    include qtscript module

--- package_qt.sh
+++ package_qt.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 MAJOR_VERSION=$1
 MINOR_VERSION=$2
 FILE=qt-everywhere-opensource-src-$MAJOR_VERSION.$MINOR_VERSION.tar.gz
@@ -9,14 +11,11 @@ cd /tmp
 echo "Downloading source package: $DOWNLOAD"
 wget -q $DOWNLOAD
 
-chmod +x /tmp/$FILE
-/tmp/$FILE
-
 echo "Extracting source package $FILE"
 mkdir qt-src
 tar zxvf $FILE > /dev/null
 cd qt-everywhere-opensource-src-$MAJOR_VERSION.$MINOR_VERSION/
-./configure --prefix=/app/.qtcs/Qt/$MAJOR_VERSION/gcc_64/ -opensource -confirm-license -no-xcb -no-compile-examples -skip qtmultimedia -skip qtsensors -skip qtscript -skip qtwebkit
+./configure --prefix=/app/.qtcs/Qt/$MAJOR_VERSION/gcc_64/ -opensource -confirm-license -no-xcb -no-compile-examples -skip qtmultimedia -skip qtsensors -skip qtwebkit
 make
 make install
 
